9f0a609ea9276412d6d8d304e2924479e7421e41
James Atkin <curlyw42@gmail.com>
2020-02-06 22:10:07 +0000

    Added docstrings

--- route_functions.py
+++ route_functions.py
@@ -3,6 +3,14 @@ import random
 
 
 def read_tsp_file(filename):
+    """Return a dictionary of integer coordinates read from a .tsp file.
+    
+    Arguments:
+        filename (string): Name of the file to read
+    
+    Returns:
+        dict: Dictionary containing the number of the node as key, and an (x, y) coordinate as value e.g. {1 : (34, 56)}
+    """
     coords = {}
     f = open(filename, "r")
     for line in f:
@@ -16,6 +24,14 @@ def read_tsp_file(filename):
     return coords
 
 def read_tour_file(filename):
+    """Return a route as read from a .tour file.
+    
+    Arguments:
+        filename(string): Name of the file to read
+    
+    Returns:
+        list: List of point numbers composing a route
+    """
     route = []
     f = open(filename, "r")
     for line in f:
@@ -29,6 +45,15 @@ def read_tour_file(filename):
     return route
 
 def calc_pseudo_euclid_dist(i, j):
+    """Calculate the pseudo-Euclidean distance between two (x, y) points.
+    
+    Arguments:
+        i (int, int): Tuple representing x and y as integers
+        j  (int, int): Tuple representing x and y as integers
+    
+    Returns:
+        int: Integer representing the calculated distance
+    """
     xdiff = i[0] - j[0]
     ydiff = i[1] - j[1]
     rij = math.sqrt((xdiff * xdiff + ydiff * ydiff) / 10.0)
@@ -38,12 +63,29 @@ def calc_pseudo_euclid_dist(i, j):
     return dij
 
 def generate_rand_route(city_coords):
+    """Generate a random route between all cities, given a dict.
+    
+    Args:
+        city_coords (dict): Dictionary containing the number of the node as key, and an (x, y) coordinate as value e.g. {1 : (34, 56)}
+    
+    Returns:
+        list: List of point numbers composing a route
+    """
     cities = city_coords.keys()
     route = random.sample(cities, len(cities))
     route.append(route[0])
     return route
 
 def calc_route_length(route, city_coords):
+    """Calculate the length of a route.
+    
+    Args:
+        route (list): List of point numbers composing a route
+        city_coords Dictionary containing the number of the node as key, and an (x, y) coordinate as value e.g. {1 : (34, 56)}
+    
+    Returns:
+        int: Length of route
+    """
     length = 0
     i = 0
     while i != len(route)-1:
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -2,16 +2,26 @@ from route_functions import *
 
 
 def get_energy(solution, city_coords):
+    """Get the energy of a solution. In the Travelling Salesman Problem, this is the length of the route given by the solution.
+    
+    Args:
+        solution (list): List of point numbers composing a route
+        city_coords (dict): Dictionary containing the number of the node as key, and an (x, y) coordinate as value e.g. {1 : (34, 56)}
+    
+    Returns:
+        int: Length of route
+    """
     return calc_route_length(solution, city_coords)
     
 def get_rand_neighbour(solution):
-    # max_pos = len(solution) - 1
-    # rand_pos_1 = random.randint(2, max_pos)
-    # rand_pos_2 = random.randint(0, max_pos - rand_pos_1)
-
-    # neighbour = solution.copy()
-    # neighbour[rand_pos_1 : (rand_pos_1 + rand_pos_2)] = reversed(neighbour[rand_pos_1 : (rand_pos_1 + rand_pos_2)])
-
+    """Get a random neighbouring solution given a particular solution. This is found by picking two random points and swapping their positions in the route.
+    
+    Args:
+        solution (list): List of point numbers composing a route
+    
+    Returns:
+        list: List of point numbers composing a route
+    """
     max_pos = len(solution) - 1
     rand_pos_1 = random.randint(0, max_pos)
     rand_pos_2 = rand_pos_1
@@ -28,15 +38,45 @@ def get_rand_neighbour(solution):
     return neighbour
 
 def calc_acceptance_prob(energy_cur, energy_new, temp):
+    """Calculate the probability needed to accept a new solution as the best solution.
+    
+    Args:
+        energy_cur (int): Energy (distance) of the current solution (route)
+        energy_new (int): Energy (distance) of the new solution (route)
+        temp (float): Temperature of the annealing process
+    
+    Returns:
+        float: The probabiliy
+    """
     if energy_new < energy_cur:
-        return 1
+        return 1.0
     else:
         return math.exp((energy_cur - energy_new) / temp)
 
 def calc_new_temperature(temp, cooling_rate):
+    """Calculate a new temperature according to the annealing schedule.
+    
+    Args:
+        temp (float): Temperature of the annealing process
+        cooling_rate (float): Factor by which to decrease the temperature
+    
+    Returns:
+        float: New temperature of the annealing process
+    """
     return temp * cooling_rate
 
 def perform_simulated_annealing(temp_initial, cooling_rate, max_iteration, city_coords):
+    """Perform simulated annealing (SA) to solve the Travelling Salesman Problem for a given group of cities.
+    
+    Args:
+        temp_initial (float): Initial temperature of the annealing process
+        cooling_rate (float): Factor by which to decrease the temperature
+        max_iteration (int): Maximum number of interations of the algorithm to perform
+        city_coords (dict): Dictionary containing the number of the node as key, and an (x, y) coordinate as value e.g. {1 : (34, 56)}
+    
+    Returns:
+        list: List of point numbers composing the optimal route found
+    """
     sol_initial = generate_rand_route(city_coords)
     sol_cur = sol_initial
     energy_cur = get_energy(sol_cur, city_coords)
